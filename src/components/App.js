468760fc4310a2646b8875df31ec110655d06df7
Atakan <atakan.dogan@gmail.com>
2023-09-29 13:37:59 +0300

    195 - Handling new answers

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,8 @@ const initialState = {
 
   // current question
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -24,16 +26,31 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const currentQuestion = state.questions[state.index];
+
+      const currentPoint =
+        currentQuestion.correctOption === action.payload
+          ? currentQuestion.points
+          : 0;
+
+      return {
+        ...state,
+        answer: action.payload,
+        points: state.points + currentPoint,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
+
+  console.log(answer);
 
   const numQuestions = questions.length;
 
@@ -53,7 +70,13 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "error" && <Error />}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
